05c669955a22fe0b2f5f7565d86befab31a4dfac
Nathan Zhang <nathan.zhang@roxburylatin.org>
2025-02-05 13:42:53 -0500

    Get Event Ids

--- API Call.py
+++ API Call.py
@@ -126,9 +126,16 @@ def getEvents():
     teams = response.json()
     return(teams)
 
+def getEventIds():
+    ids = []
+    for event in getEvents().get('data'):
+        ids.append(event.get('id'))
+    return ids
+
 def get_match_scores(teamid):
     matches = getTeamScores(teamid)
     matches = matches.get('data')
+    match_scores = []
     for match in matches:
         temp_match_scores = []
         match_info = match.get("alliances")[0:2]
@@ -151,7 +158,7 @@ match_scores = []
             #match_scores.append(match)
 
 #print(match_scores)
-pprint(getEvents())
+print(getEventIds())
 
 
 
